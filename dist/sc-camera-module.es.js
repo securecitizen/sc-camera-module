427f1fabf209033bca125d32e4e7512802145d50
Grant Vine <grantv@securecitizen.co.za>
2023-07-12 11:34:11 +0200

    no message

--- dist/sc-camera-module.es.js
+++ dist/sc-camera-module.es.js
@@ -2,9 +2,9 @@
  * name: sc-camera-module
  * version: v1.0.0
  * description: This is the SC Camera Module repo that will create a Vite workflow to ease creation of Javascript modules with a dev server, GitHub Pages support and automated publishing to NPM.
- * author: Grant Vine <grantv@securecitizen.co.za> (https://securecityizen.co.za)
+ * author: Grant Vine <grantv@securecitizen.co.za> (https://securecitizen.co.za)
  * repository: https://github.com/securecitizen/sc-camera-module
- * build date: 2023-07-12T09:08:45.223Z 
+ * build date: 2023-07-12T09:32:57.933Z 
  */
 function init(message) {
   console.log(message);
--- dist/sc-camera-module.umd.js
+++ dist/sc-camera-module.umd.js
@@ -2,9 +2,9 @@
  * name: sc-camera-module
  * version: v1.0.0
  * description: This is the SC Camera Module repo that will create a Vite workflow to ease creation of Javascript modules with a dev server, GitHub Pages support and automated publishing to NPM.
- * author: Grant Vine <grantv@securecitizen.co.za> (https://securecityizen.co.za)
+ * author: Grant Vine <grantv@securecitizen.co.za> (https://securecitizen.co.za)
  * repository: https://github.com/securecitizen/sc-camera-module
- * build date: 2023-07-12T09:08:45.223Z 
+ * build date: 2023-07-12T09:32:57.933Z 
  */
 (function(global, factory) {
   typeof exports === "object" && typeof module !== "undefined" ? module.exports = factory() : typeof define === "function" && define.amd ? define(factory) : (global = typeof globalThis !== "undefined" ? globalThis : global || self, global["sc-camera-module"] = factory());
