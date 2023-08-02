317c0d52f47d495a6bb7dd5e591b5d839883bd7a
Grant Vine <grantv@securecitizen.co.za>
2023-08-02 11:27:06 +0200

    trying

--- vite.config.js
+++ vite.config.js
@@ -26,20 +26,6 @@ module.exports = defineConfig({
         dts({
             insertTypesEntry: true,
         }),
-        nodePolyfills({
-            // To exclude specific polyfills, add them to this list.
-            // exclude: [
-            //   'fs', // Excludes the polyfill for `fs` and `node:fs`.
-            // ],
-            // Whether to polyfill specific globals.
-            globals: {
-              Buffer: true, // can also be 'build', 'dev', or false
-              global: true,
-              process: true,
-            },
-            // Whether to polyfill `node:` protocol imports.
-            protocolImports: true,
-          }),
         banner(
             `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * repository: ${pkg.repository.url}\n * build date: ${now} \n */`
         ),
